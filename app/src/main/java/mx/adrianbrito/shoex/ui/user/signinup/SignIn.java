2338384ef929b39b044630a897206d844232a246
adrianbrito <a_brito88@hotmail.com>
2017-03-03 16:20:13 -0600

    Se crea pantalla Init, SignIn y SignUp

---
+++ app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/SignIn.java
@@ -0,0 +1,14 @@
+package mx.adrianbrito.shoex.ui.user.signinup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import mx.adrianbrito.shoex.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
