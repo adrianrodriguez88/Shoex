ac6deb95c8d7802ef7052ae2b8e365028e8ed499
adrianbrito <a_brito88@hotmail.com>
2017-03-03 19:28:50 -0600

    Pruebas

--- app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/SignIn.java
+++ app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/SignIn.java
@@ -2,14 +2,26 @@ package mx.adrianbrito.shoex.ui.user.signinup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
 import mx.adrianbrito.shoex.R;
 
 public class SignIn extends AppCompatActivity {
+
+    private EditText txtUser;
+    private EditText txtPassword;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        txtUser = (EditText) findViewById(R.id.txtUser);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+        toolbar = (Toolbar) findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
 
     }
 }
