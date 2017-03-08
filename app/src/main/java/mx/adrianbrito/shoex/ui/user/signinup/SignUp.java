15cb802e135f6c78b008a04c1b30b83bb272becb
adrianbrito <a_brito88@hotmail.com>
2017-03-07 20:15:57 -0600

    Pruebas en SignUp.

--- app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/SignUp.java
+++ app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/SignUp.java
@@ -4,19 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import mx.adrianbrito.shoex.R;
 
 public class SignUp extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText txtGname;
+    private EditText txtFname;
+    private EditText txtSFname;
+    private View scrollView;
+    private Button btnAccept;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         this.initToolbar();
+        txtGname = (EditText) findViewById(R.id.txtGname);
+        txtFname = (EditText) findViewById(R.id.txtFname);
+        txtSFname = (EditText) findViewById(R.id.txtSFname);
 
+        scrollView = findViewById(R.id.scrollview_signup);
+
+        btnAccept = (Button) findViewById(R.id.btnAccept);
     }
 
     public void initToolbar() {
