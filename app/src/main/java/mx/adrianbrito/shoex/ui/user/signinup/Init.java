93258d236f22d4bfb5956d450c1ef2a25f3faf21
adrianbrito <a_brito88@hotmail.com>
2017-03-16 19:31:10 -0600

    SMSValidation - Start.

--- app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/Init.java
+++ app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/Init.java
@@ -35,7 +35,7 @@ public class Init extends AppCompatActivity {
                 Intent intent =
                         new Intent(mx.adrianbrito.shoex.ui.user.signinup.Init.this,
                                 //mx.adrianbrito.shoex.ui.user.signinup.SignIn.class);
-                                mx.adrianbrito.shoex.ui.user.signinup.PhoneValidation.class);
+                                mx.adrianbrito.shoex.ui.user.signinup.SMSValidation.class);
 
                 startActivity(intent);
             }
---
+++ app/src/main/java/mx/adrianbrito/shoex/ui/user/signinup/SMSValidation.java
@@ -0,0 +1,29 @@
+package mx.adrianbrito.shoex.ui.user.signinup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.adrianbrito.shoex.R;
+
+public class SMSValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smsvalidation);
+        this.initToolbar();
+    }
+
+    private void initToolbar(){
+        getSupportActionBar().setTitle("Registro 4 de 5");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+}
